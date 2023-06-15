47df5d17e6bf342ab2f016981e86a597fcb22752
RichardAcheampong <richard.ach@yahoo.com>
2023-06-15 12:52:41 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a command that prints c program using the printf funtion
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
