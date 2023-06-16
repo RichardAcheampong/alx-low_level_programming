f4c2460df0b8e08e1aabde3b97e486bea7996edc
RichardAcheampong <richard.ach@yahoo.com>
2023-06-15 18:21:49 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return: Always Return 1 (Success)
- */
-char main(void)
+*main - Entry point
+*Return: 1
+*/
+int main(void)
 {
-	fput("and that place of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
